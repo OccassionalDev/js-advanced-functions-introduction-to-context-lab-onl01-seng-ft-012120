89254a02937ab4fed5e68c0381ef71f8276d7f53
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-12 02:05:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ let createEmployeeRecords = function (employeeData) {
 let createTimeInEvent = function (employee, dateStamp) {
   let [date, hour] = dateStamp.split(" ");
   
-  return employee.timeInEvents.push(
+  return employee.timeInEvents.push({
       type: "TimeIn",
       hour: parseInt(hour, 10),
       date
-    );
+    });
 };
